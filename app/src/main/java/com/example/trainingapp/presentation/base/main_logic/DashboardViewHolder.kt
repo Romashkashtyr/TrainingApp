8413983f3ef06ecbc84f7796aae4c3b8e56601db
Romashka <romainshtyr@yandex.ru>
2024-11-25 11:47:32 +0300

    rc_view

---
+++ app/src/main/java/com/example/trainingapp/presentation/base/main_logic/DashboardViewHolder.kt
@@ -0,0 +1,44 @@
+package com.example.trainingapp.presentation.base.main_logic
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.example.trainingapp.databinding.ItemStepsBinding
+import com.example.trainingapp.databinding.ItemTrainingListBinding
+import com.example.trainingapp.databinding.ItemWaterBinding
+import com.example.trainingapp.databinding.ItemWorkoutBinding
+import com.example.trainingapp.domain.DashboardItem
+
+sealed class DashboardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    inner class StepsViewHolder(private val binding: ItemStepsBinding) : DashboardViewHolder(binding.root) {
+        fun bind(item: DashboardItem.StepsItem){
+            binding.stepsCount.text = item.stepsCount.toString()
+        }
+    }
+
+    inner class WaterViewHolder(
+        private val binding: ItemWaterBinding,
+        private val onAddWaterClicked: () -> Unit
+    ) : DashboardViewHolder(binding.root){
+        fun bind(item: DashboardItem.WaterItem){
+            binding.waterIntake.text = item.waterCount.toString()
+            binding.addWaterButton.setOnClickListener { onAddWaterClicked() }
+        }
+    }
+
+    inner class WorkoutsViewHolder(private val binding: ItemWorkoutBinding) : DashboardViewHolder(binding.root){
+        fun bind(item: DashboardItem.WorkoutItem){
+            binding.workoutsCount.text = item.workoutCounts.toString()
+        }
+    }
+
+    inner class TrainingListViewHolder(
+        private val binding: ItemTrainingListBinding,
+        private val onViewTrainingsClicked: () -> Unit
+    ) : DashboardViewHolder(binding.root){
+        fun bind(item: DashboardItem.TrainingListItem){
+            binding.viewWorkoutsButton.setOnClickListener { onViewTrainingsClicked() }
+        }
+    }
+}
