14b6f7568fea8e0d4bf0fbca2757ba2e83aacb58
Romashkashtyr <Romainshtyr@yandex.ru>
2025-02-11 15:30:36 +0300

    fixes january 29.01

--- app/src/main/java/com/example/trainingapp/presentation/base/trainings/TrainingsListActivity.kt
+++ app/src/main/java/com/example/trainingapp/presentation/base/trainings/TrainingsListActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trainingapp.R
 import com.example.trainingapp.databinding.ActivityTrainingsListBinding
+import com.example.trainingapp.domain.di.ApplicationComponent
 import com.example.trainingapp.presentation.base.BaseActivity
 
 class TrainingsListActivity : BaseActivity(), TrainingsView {
     private lateinit var binding: ActivityTrainingsListBinding
+
+    private val component by lazy {
+        
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTrainingsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         }
+
+    override fun showTrainingsList() {
+        TODO("Not yet implemented")
+    }
 
 }
