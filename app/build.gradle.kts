04e422ffc159f2806db6c079a7f0ca6e85e997b8
Romashkashtyr <romainshtyr@yandex.ru>
2025-02-24 21:51:29 +0300

    Firebase - dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation (libs.firebase.database)
     implementation (libs.firebase.core)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
 
 
     implementation(libs.androidx.core.ktx)
---
+++ app/src/main/java/com/example/trainingapp/presentation/trainings/TrainingsView.kt
@@ -0,0 +1,12 @@
+package com.example.trainingapp.presentation.trainings
+
+import com.example.trainingapp.presentation.base.BaseView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+
+@StateStrategyType(value = AddToEndSingleStrategy::class)
+interface TrainingsView: BaseView {
+
+    fun showTrainingsList()
+}
