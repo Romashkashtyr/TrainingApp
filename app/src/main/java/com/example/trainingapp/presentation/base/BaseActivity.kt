fa1e1876e8fcd164d25a57691459de13d123430a
Romashkashtyr <Romainshtyr@yandex.ru>
2025-07-21 15:03:10 +0300

    fixing2

--- app/src/main/java/com/example/trainingapp/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/example/trainingapp/presentation/base/BaseActivity.kt
@@ -5,7 +5,7 @@ import moxy.MvpAppCompatActivity
 
 open class BaseActivity : MvpAppCompatActivity(), BaseView  {
 
-    override fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    override fun showToast(message: Int) {
+        Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
     }
 }
