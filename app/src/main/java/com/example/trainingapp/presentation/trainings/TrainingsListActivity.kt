b8aa865081c2268695464fa4f2d12a38216badc7
Romashkashtyr <Romainshtyr@yandex.ru>
2025-07-22 10:38:12 +0300

    fixing3

--- app/src/main/java/com/example/trainingapp/presentation/trainings/TrainingsListActivity.kt
+++ app/src/main/java/com/example/trainingapp/presentation/trainings/TrainingsListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.trainingapp.presentation.trainings
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trainingapp.databinding.ActivityTrainingsListBinding
@@ -7,6 +8,7 @@ import com.example.trainingapp.domain.Training
 import com.example.trainingapp.domain.di.modules.TrainingFactory
 import com.example.trainingapp.presentation.TrainingApp
 import com.example.trainingapp.presentation.base.BaseActivity
+import com.example.trainingapp.presentation.main.MainActivity
 import com.example.trainingapp.presentation.trainings.rc_view_training.TrainingAdapter
 import moxy.ktx.moxyPresenter
 import javax.inject.Inject
@@ -19,10 +21,7 @@ class TrainingsListActivity : BaseActivity(), TrainingsView {
     @Inject
     lateinit var trainingFactory: TrainingFactory
 
-   // private val presenter by moxyPresenter { TrainingsPresenter() }
     private val presenter by moxyPresenter { trainingFactory.createTrainingPresenter() }
-
-
 
 
     init {
@@ -36,12 +35,17 @@ class TrainingsListActivity : BaseActivity(), TrainingsView {
         binding = ActivityTrainingsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         presenter.requestTrainingList()
+
+        binding.arrowBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
 
     override fun showTrainingsList(trainingList: List<Training>) {
-            trainingAdapter = TrainingAdapter(trainingList)
-            binding.rcViewTraining.apply {
+        trainingAdapter = TrainingAdapter(trainingList)
+        binding.rcViewTraining.apply {
             layoutManager = LinearLayoutManager(this@TrainingsListActivity)
             adapter = trainingAdapter
         }
