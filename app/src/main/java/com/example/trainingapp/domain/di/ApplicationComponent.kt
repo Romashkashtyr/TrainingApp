41e3b533ec7fdf03c549b55734985e4f5e4a228d
Romashkashtyr <Romainshtyr@yandex.ru>
2025-10-22 15:25:18 +0300

    features modules2

--- app/src/main/java/com/example/trainingapp/domain/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/trainingapp/domain/di/ApplicationComponent.kt
@@ -1,15 +1,15 @@
 package com.example.trainingapp.domain.di
 
 import android.content.Context
+import com.example.auth.ui.AuthorizationActivity
+import com.example.splash.ui.SplashActivity
 import com.example.trainingapp.domain.di.modules.AppModule
 import com.example.trainingapp.domain.di.modules.AuthModule
 import com.example.trainingapp.domain.di.modules.ExceptionModule
 import com.example.trainingapp.domain.di.modules.FitnessResultModule
 import com.example.trainingapp.domain.di.modules.NetworkModule
 import com.example.trainingapp.domain.di.modules.TrainingModule
-import com.example.auth.AuthorizationActivity
-import com.example.splash.ui.SplashActivity
-import com.example.trainings.TrainingsListActivity
+import com.example.trainings.ui.TrainingsListActivity
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -27,8 +27,8 @@ import javax.inject.Singleton
 )
 interface ApplicationComponent {
 
-    fun inject(activity: com.example.trainings.TrainingsListActivity)
-    fun inject(activity: com.example.auth.AuthorizationActivity)
+    fun inject(activity: TrainingsListActivity)
+    fun inject(activity: AuthorizationActivity)
     fun inject(activity: SplashActivity)
 
 
