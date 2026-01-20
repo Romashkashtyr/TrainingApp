c3a9333b29300a42f87bba79d340ab759571ae31
Romashkashtyr <Romainshtyr@yandex.ru>
2026-01-20 11:39:19 +0300

    features modules3

--- auth/src/main/java/com/example/auth/ui/AuthorizationPresenter.kt
+++ auth/src/main/java/com/example/auth/ui/AuthorizationPresenter.kt
@@ -14,37 +14,10 @@ import javax.inject.Inject
 open class AuthorizationPresenter @Inject constructor(
     private val signInUseCase: SignInUseCase,
     private val signUpUseCase: SignUpUseCase
-    //private val authRepository: AuthRepository
 ) : BasePresenter<AuthorizationView>() {
 
 
     fun signIn(email: String, password: String) {
-//        if (email.isNotEmpty() && password.isNotEmpty()) {
-//            viewState?.showViewProgress()
-//            launch {
-//                val authStatus = authRepository.signIn(email, password)
-//                withContext(Dispatchers.Main) {
-//                    when (authStatus) {
-//                        is Status.Failure -> {
-//                            viewState.showToast(R.string.sign_in_failure)
-//                        }
-//
-//                        is Status.NoNetwork -> {
-//                            viewState.showToast(R.string.network_failure)
-//                        }
-//
-//                        is Status.Success -> {
-//                            viewState.showToast(R.string.sign_in_success)
-//                            viewState.navigateToHome()
-//                        }
-//                    }
-//                    viewState?.hideViewProgress()
-//                }
-//
-//            }
-//        } else {
-//            viewState.showToast(R.string.enter_your_data)
-//        }
 
         viewState.showViewProgress()
         launch {
@@ -86,43 +59,12 @@ open class AuthorizationPresenter @Inject constructor(
                 }
             }
         }
-//        if (email.isEmpty() || password.isEmpty()) {
-//            viewState.showToast(R.string.sign_in_failure)
-//            return
-//        }
-//        if (password.length < 6) {
-//            viewState.showToast(R.string.sign_up_weak_password)
-//            return
-//        }
-//        viewState?.showViewProgress()
-//        launch {
-//            val authStatus = authRepository.signUp(email, password, confirmPassword)
-//            withContext(Dispatchers.Main) {
-//                when (authStatus) {
-//                    is Status.Failure -> {
-//                        viewState.showToast(R.string.sign_in_failure)
-//                    }
-//
-//                    is Status.NoNetwork -> {
-//                        viewState.showToast(R.string.network_failure)
-//                    }
-//
-//                    is Status.Success -> {
-//                        viewState.showToast(R.string.sign_in_success)
-//                        viewState.navigateToHome()
-//                    }
-//                }
-//                viewState?.hideViewProgress()
-//            }
-//        }
-
     }
 
 
     fun requestChangeMode() {
         viewState.changeAuthMode()
     }
-
 
 }
 
